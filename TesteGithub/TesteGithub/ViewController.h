7fceb36d911243d622fc488cedb9f5fc1215c512
rrovaron <rodrigorovaron@MacBook-Pro-de-Rodrigo-2.local>
2013-10-25 16:14:56 -0200

    Add reconhecimento de frequencia
    foram add os arquivos
    -> audioController
    -> PitchDetector
    ->TPCircularBuffer
    
    e Alterado a ViewControler e Main.storyBoard

--- TesteGithub/TesteGithub/ViewController.h
+++ TesteGithub/TesteGithub/ViewController.h
@@ -7,11 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AudioController.h"
+#import "PitchDetector.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <PitchDetectorDelegate, AudioControllerDelegate>
+{
+    UILabel *freqLabel;
+    AudioController *audioManager;
+    PitchDetector *autoCorrelator;
+    
+    NSMutableArray *medianPitchFollow;
+}
+
+
 @property (strong, nonatomic) IBOutlet UIImageView *proxNota;
 @property (strong, nonatomic) IBOutlet UIImageView *nota;
 @property (strong, nonatomic) IBOutlet UIImageView *antNota;
+@property (strong, nonatomic) IBOutlet UILabel *lValorFreq;
 
 - (IBAction)start:(id)sender;
 @end
--- TesteGithub/TesteGithub/ViewController.m
+++ TesteGithub/TesteGithub/ViewController.m
@@ -18,6 +18,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    audioManager = [AudioController sharedAudioManager];
+    audioManager.delegate = self;
+    autoCorrelator = [[PitchDetector alloc] initWithSampleRate:audioManager.audioFormat.mSampleRate lowBoundFreq:30 hiBoundFreq:4500 andDelegate:self];
+    
+    medianPitchFollow = [[NSMutableArray alloc] initWithCapacity:22];
+    
+    
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,6 +44,60 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+- (void) updatedPitch:(float)frequency {
+    
+    double value = frequency;
+    
+    
+    
+    //############ DATA SMOOTHING ###############
+    //###     The following code averages previous values  ##
+    //###  received by the pitch follower by using a             ##
+    //###  median filter. Provides sub cent precision!          ##
+    //#############################################
+    
+    NSNumber *nsnum = [NSNumber numberWithDouble:value];
+    [medianPitchFollow insertObject:nsnum atIndex:0];
+    
+    if(medianPitchFollow.count>22) {
+        [medianPitchFollow removeObjectAtIndex:medianPitchFollow.count-1];
+    }
+    double median = 0;
+    
+    
+    
+    if(medianPitchFollow.count>=2) {
+        NSSortDescriptor *highestToLowest = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO];
+        NSMutableArray *tempSort = [NSMutableArray arrayWithArray:medianPitchFollow];
+        [tempSort sortUsingDescriptors:[NSArray arrayWithObject:highestToLowest]];
+        
+        if(tempSort.count%2==0) {
+            double first = 0, second = 0;
+            first = [[tempSort objectAtIndex:tempSort.count/2-1] doubleValue];
+            second = [[tempSort objectAtIndex:tempSort.count/2] doubleValue];
+            median = (first+second)/2;
+            value = median;
+        } else {
+            median = [[tempSort objectAtIndex:tempSort.count/2] doubleValue];
+            value = median;
+        }
+        
+        [tempSort removeAllObjects];
+        tempSort = nil;
+    }
+
+    self.lValorFreq.text = [NSString stringWithFormat:@"%3.1f Hz", value];
+
+    
+}
+
+- (void) receivedAudioSamples:(SInt16 *)samples length:(int)len {
+    [autoCorrelator addSamples:samples inNumberFrames:len];
+}
+
 - (IBAction)start:(id)sender {
+    
 }
 @end
