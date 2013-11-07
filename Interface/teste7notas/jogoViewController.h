93fb90c9444845b4763776bcec4345f1befca9ff
Bruno Vargas <BrunoVargas@MacBook-Pro-de-Bruno.local>
2013-11-07 10:27:39 -0200

    mudancas no jogo

--- Interface/teste7notas/jogoViewController.h
+++ Interface/teste7notas/jogoViewController.h
@@ -15,9 +15,16 @@
 @property (nonatomic, strong) NotaMusical *notaAntiga;
 @property (nonatomic, strong) NotaMusical *notaCorrente;
 @property (nonatomic, strong) NSTimer *temporizador;
+@property (weak, nonatomic) IBOutlet UIButton *botaoParar;
+@property (weak, nonatomic) IBOutlet UIButton *botaoComecar;
+@property (weak, nonatomic) IBOutlet UILabel *notaDesafio;
+@property (weak, nonatomic) IBOutlet UILabel *notaTocada;
+@property (weak, nonatomic) IBOutlet UIProgressView *tempo;
 
 - (void) jogar;
-- (void) novoDesafio;
+- (void) novoDesafio;//cria uma nova nota musical para ser utilizada no desafio. Esata nota eh verificada para que nao seja igual a anterior!
 - (BOOL) verificarNota;
+- (IBAction)comecarJogo:(id)sender;
+- (IBAction)pararJogo:(id)sender;
 
 @end
--- Interface/teste7notas/jogoViewController.m
+++ Interface/teste7notas/jogoViewController.m
@@ -39,10 +39,51 @@
 
 }
 - (void) novoDesafio{
+    int auxiliar = arc4random() % 7;
+    switch (auxiliar) {
+        case 0 : //do
+            if (![self.notaAntiga.nome isEqualToString:@"do"])
+                self.notaCorrente = [[NotaMusical alloc] initWithValor:@"do" andFrequencia:32.700 andNumOitava:1];
+                break;
+        case 1: //re
+            if (![self.notaAntiga.nome isEqualToString:@"re"])
+                self.notaCorrente = [[NotaMusical alloc] initWithValor:@"re" andFrequencia:36.6875 andNumOitava:1];
+            
+            break;
+        case 2: //mi
+            if (![self.notaAntiga.nome isEqualToString:@"mi"])
+                self.notaCorrente = [[NotaMusical alloc] initWithValor:@"mi" andFrequencia:41.200 andNumOitava:1];
+            
+            break;
+        case 3: //fa
+            if (![self.notaAntiga.nome isEqualToString:@"fa"])
+                self.notaCorrente = [[NotaMusical alloc] initWithValor:@"fa" andFrequencia:43.65 andNumOitava:1];
+            
+            break;
+        case 4: //sol
+            if (![self.notaAntiga.nome isEqualToString:@"sol"])
+                self.notaCorrente = [[NotaMusical alloc] initWithValor:@"sol" andFrequencia:49.00 andNumOitava:1];
+            break;
+        case 5: //la
+            if (![self.notaAntiga.nome isEqualToString:@"la"])
+                self.notaCorrente = [[NotaMusical alloc] initWithValor:@"la" andFrequencia:55.00 andNumOitava:1];
+            
+            break;
+        case 6: //si
+            if (![self.notaAntiga.nome isEqualToString:@"si"])
+                self.notaCorrente = [[NotaMusical alloc] initWithValor:@"si" andFrequencia:61.725 andNumOitava:1];
+            break;
+    }
 
 }
 - (BOOL) verificarNota{
     return TRUE;
+}
+
+- (IBAction)comecarJogo:(id)sender {
+}
+
+- (IBAction)pararJogo:(id)sender {
 }
 
 
